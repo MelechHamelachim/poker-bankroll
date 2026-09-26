bbf5ae72e89ffad8f7db1b8e8ffa50afa6e19ac5
MelechHamelachim <pauloroberto23023@gmail.com>
2026-09-26 13:12:21 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'poker-bankroll-v3';
+const CACHE = 'poker-bankroll-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './sw.js', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
